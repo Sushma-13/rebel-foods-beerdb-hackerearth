47d19861e75df34ef08567952a5d419821e5ee28
Sushma Goswami <goswamisush13@gmail.com>
2020-11-29 22:44:31 +0530

    added PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BeerBrandService } from './services/beer-brand.service';
 import { BeerComponent } from './pages/beer/beer.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, BeerComponent, ProductCardComponent],
-  imports: [BrowserModule, HttpClientModule, HttpClientJsonpModule],
+  imports: [BrowserModule, HttpClientModule, HttpClientJsonpModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [BeerBrandService],
   bootstrap: [AppComponent],
 })
